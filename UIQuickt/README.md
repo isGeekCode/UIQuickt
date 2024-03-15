4464c29ed55115e6974a629bc7d3c8dacd1cba55
isGeekCode <h1guitar@naver.com>
2024-03-15 11:29:39 +0900

    add UIQuickt

---
+++ UIQuickt/README.md
@@ -0,0 +1,16 @@
+
+
+
+
+
+```swift
+import SwiftUI
+
+@available(iOS 13, *)
+struct SomeViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        preview(<#SomeViewController#>())
+    }
+}
+
+```
---
+++ UIQuickt/Sources/UIQuickt/UIQuickt.swift
@@ -0,0 +1,60 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+//
+//  UIQuickt.swift
+//  
+//
+//  Created by bang_hyeonseok aka GeekCode on 3/15/24.
+//
+
+import SwiftUI
+import UIKit
+
+// MARK: - UIViewController 확장
+@available(iOS 13, *)
+public extension UIViewController {
+    func toPreview() -> some View {
+        UIViewControllerRepresentablePreview(viewController: self)
+    }
+}
+
+// MARK: - UIViewController 래퍼
+@available(iOS 13, *)
+public struct UIViewControllerRepresentablePreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+    private var viewController: ViewController
+    
+    public init(viewController: ViewController) {
+        self.viewController = viewController
+    }
+    
+    public func makeUIViewController(context: Context) -> ViewController {
+        viewController
+    }
+    
+    public func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    }
+}
+
+// MARK: - UIView 확장
+@available(iOS 13, *)
+public extension UIView {
+    func toPreview() -> some View {
+        UIViewRepresentablePreview(view: self)
+    }
+}
+
+// MARK: - UIView 래퍼
+@available(iOS 13, *)
+public struct UIViewRepresentablePreview<View: UIView>: UIViewRepresentable {
+    private var view: View
+    
+    public init(view: View) {
+        self.view = view
+    }
+    
+    public func makeUIView(context: Context) -> View {
+        view
+    }
+    
+    public func updateUIView(_ uiView: View, context: Context) {}
+}
